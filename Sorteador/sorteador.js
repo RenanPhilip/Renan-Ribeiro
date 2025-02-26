ae8bea9edfe34625556212b9ae469a90bb4d0f6d
Renan Ribeiro <rphiliper@gmail.com>
2025-02-26 00:39:31 -0300

    chore: no code changes made

--- Sorteador/sorteador.js
+++ Sorteador/sorteador.js
@@ -1,30 +1,43 @@
 let listaDeCoisas = [];
 
+// Função para adicionar um item à lista
 function adicionarItem() {
     const itemInput = document.getElementById('item');
     const item = itemInput.value.trim();
     const resultado = document.getElementById('resultado');
     
     if (item === '') {
-        //alert('Por favor, digite um item válido!');
+        // Se o item estiver vazio, não faz nada
         return;
     }
 
     if (listaDeCoisas.includes(item)) {
+        // Se o item já estiver na lista, remove-o
         listaDeCoisas = listaDeCoisas.filter(i => i !== item);
         resultado.textContent = `Item "${item}" removido da lista!`;
+        itemInput.value = "";
     } else {
+        // Se o item não estiver na lista, adiciona-o
         listaDeCoisas.push(item);
         resultado.textContent = `Item "${item}" adicionado à lista!`;
+        itemInput.value = "";
     }
 
     // Remove itens vazios da lista
     listaDeCoisas = listaDeCoisas.filter(item => item !== '');
 
     atualizarLista();
-    itemInput.value = "";
 }
 
+// Adicionar função para remover um item da lista por um botão, sem a necessidade de digitar o item novamente
+function removerdaLista() {
+    listaDeCoisas = listaDeCoisas.filter(i => i !== item);
+    resultado.textContent = `Item "${item}" removido da lista!`;
+    
+    atualizarLista();
+}
+
+// Função para atualizar a lista exibida na tela
 function atualizarLista() {
     const lista = document.getElementById('lista');
     lista.innerHTML = '';
@@ -36,6 +49,7 @@ function atualizarLista() {
     });
 }
 
+// Função para sortear um item da lista
 function sortear() {
     if (listaDeCoisas.length === 0) {
         alert('Adicione itens antes de sortear!');
@@ -47,17 +61,12 @@ function sortear() {
 
     // Efeito de "sorteando"
     setTimeout(() => {
-        // if (Math.random() < 0.5) {
-        //     resultado.textContent = 'Vamos fazer amorzinho gostoso!!';
-        //     resultado.innerHTML += '<br>HMMMMMM!!!';
-        // } else {
-            const indice = Math.floor(Math.random() * listaDeCoisas.length);
-            resultado.textContent = `Item sorteado: ${listaDeCoisas[indice]}`;
-        // }
+        const indice = Math.floor(Math.random() * listaDeCoisas.length);
+        resultado.textContent = `Item sorteado: ${listaDeCoisas[indice]}`;
     }, 500);
 }
 
-// Adiciona event listener para a tecla Enter
+// Adiciona event listener para a tecla Enter no campo de input
 document.getElementById('item').addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
         adicionarItem();
