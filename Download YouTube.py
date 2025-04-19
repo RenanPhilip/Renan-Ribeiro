d30dca2a7ef688a43ce22ba8d391bd0f50f1b92c
Renan Ribeiro <rphiliper@gmail.com>
2025-04-19 13:52:02 -0300

    Remove Download YouTube.py script

--- Download YouTube.py
+++
@@ -1,8 +0,0 @@
-from pytube import *
-
-yt = YouTube(input('Cole aqui sua url: *))
-
-print(yt.title) & Para descobrir o título do vídeo
-print(yt.thumbnail url) & Para receber a url da thumb
-stream = yt.streams.get highest resolution() %* Melhor reso!
-stream.download()
