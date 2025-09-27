7ac1d79e70f68ddd3fe0e8e57333c0ae6ab5cc75
Renan Ribeiro <rphiliper@gmail.com>
2025-09-27 10:03:48 -0300

    Improve task removal functionality

--- TO DO List/src/javascript/script.js
+++ TO DO List/src/javascript/script.js
@@ -1,27 +1,28 @@
 addNewTask = document.querySelector('#addNewTask')
 listEl = document.querySelector('#List')
-taskNumer = 0
+removeTask = document.querySelector('#removeTask')
 
-        // <div id="task" class="task">
-        //     <input type="checkbox">
-        //         Tarefa 02
-        //     <input type="button" id="removeTask" value="Delete">
-        // </div>
+taskNumer = 0
 
 newTask = (taskData) => {
     newDiv = document.createElement('div')
     newDiv.id = `task${taskNumer}`   
     newDiv.className = "task"
 
-    newDiv.innerHTML = `<input type="checkbox">
+    newDiv.innerHTML = `<input type="checkbox" id="checkbox">
     ${taskData}
     <input type="button" id="removeTask" value="Delete">`
     listEl.appendChild(newDiv)
 
     taskNumer++
 }
+
 addNewTask.addEventListener('click',()=>{
     task = document.querySelector('#newTask').value
     task != '' ? newTask(task) : console.log(task)
     document.querySelector('#newTask').value = ""
+})
+
+removeTask.addEventListener('click',(e)=>{
+    e.target.parentElement.remove()
 })
